05d5ec3760f413a732b8071e7f6ba9fc77ec907a
Artur Angiel <artur.angiel@wolterskluwer.com>
2023-07-31 10:03:56 +0200

    flutter-mx interop problem

---
+++ android/app/src/main/kotlin/com/example/cljd_hello_flutter_mx/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cljd_hello_flutter_mx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/cljd_interop_flutter_mx_problem/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cljd_interop_flutter_mx_problem
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
